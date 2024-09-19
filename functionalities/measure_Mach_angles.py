54219f6ad6f76776255f28fed7532fd28c8fa404
mjodorczuk <michaljanodorczuk@gmail.com>
2024-09-19 08:11:53 +0200

    cone boundary changed from y- to x- samples. Script adapted to the HPC

--- functionalities/measure_Mach_angles.py
+++ functionalities/measure_Mach_angles.py
@@ -1,13 +1,26 @@
 import h5py
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from numpy.linalg import lstsq
 import os
 import shutil
 import sys
+import traceback
 
+# Path to the folder containing the data
 path = sys.argv[1]
 
+# There are problems with generating plots on the server
+# Change to 'True' if you want to generate plots
+PLOT = True
+
+# How many initial timesteps to skip when estimating Mach number
+# Should be changed to some stability criterion
+skip_first = 10
+
+# Load the data
 data_ion = h5py.File(path + '/nd1p00_0000.h5', 'r')
 data_ion = data_ion[list(data_ion.keys())[0]]
 data_ele = h5py.File(path + '/nd2p00_0000.h5', 'r')
@@ -15,8 +28,16 @@ data_ele = data_ele[list(data_ele.keys())[0]]
 data_phi = h5py.File(path + '/phisp00_0000.h5', 'r')
 data_phi = data_phi[list(data_phi.keys())[0]]
 
+# Ions and electrons should have the same set of timest
 timesteps = list(data_ion.keys())
 
+# Find a point on the cone's edge at coordinate x
+# slice - 1D array of the field data
+# vs - values of density/intensity for linear regression
+# x - x coordinate of the slice
+# threshold - threshold value for the edge
+# upper - if True, find the upper edge, otherwise the lower edge
+# edge - array to store the edge points
 def trailEdgePoint(slice, vs, x, threshold, upper, edge):
     try:
         if upper:
@@ -26,10 +47,10 @@ def trailEdgePoint(slice, vs, x, threshold, upper, edge):
             ys = slice.shape[0] - np.array([np.where(slice < v)[0][0] for v in vs]) - 1
 
         M = ys[:, np.newaxis] ** [0, 1]
-        c, m = lstsq(M, vs, rcond=None)[0]
-
+        c, m = lstsq(M, vs)[0]
         edge.append([x, (threshold - c) / m])
-    except:
+    except Exception:
+        # print(traceback.format_exc())
         pass
 
 def getEdges(data, n_smooth, vs, threshold, x0, x1):
@@ -37,7 +58,7 @@ def getEdges(data, n_smooth, vs, threshold, x0, x1):
     edge2 = []
     midz = data.shape[0] // 2
     z0 = midz - n_smooth // 2
-    z1 = midz + n_smooth // 2
+    z1 = midz + n_smooth // 2 + 1
 
     data = data[z0:z1, :, :]
     data = np.average(data, axis=0)
@@ -53,38 +74,46 @@ def getEdges(data, n_smooth, vs, threshold, x0, x1):
 
 def getMach(edge):
     M = edge[:,0][:, np.newaxis] ** [0, 1]
-    c, m = lstsq(M, edge[:,1], rcond=None)[0]
+    c, m = lstsq(M, edge[:,1])[0]
     return np.abs(1 / np.sin(np.arctan(m))), c, m
 
 
 def plotCones(data, edge1, edge2, c1, m1, c2, m2, Ma1, Ma2, timestep, type):
-    plt.imshow(data)
-    plt.scatter(edge1[:,0], edge1[:,1])
-    plt.scatter(edge2[:,0], edge2[:,1])
-    plt.plot(edge1[:,0], edge1[:,0] * m1 + c1, color='k', label=f'Upper edge Ma={Ma1:.3f}')
+    if PLOT:
+        plt.imshow(data)
+        plt.scatter(edge1[:,0], edge1[:,1])
+        plt.scatter(edge2[:,0], edge2[:,1])
+        plt.plot(edge1[:,0], edge1[:,0] * m1 + c1, color='k', label=f'Upper edge Ma={Ma1:.3f}')
 
 
-    plt.plot(edge2[:,0], edge2[:,0] * m2 + c2, color='brown', label=f'Lower edge Ma={Ma2:.3f}')
-    plt.title("Frame " + timestep + " - " + type)
-    plt.legend()
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.savefig(f'plots/{type}/{timestep}')
-    plt.clf()
+        plt.plot(edge2[:,0], edge2[:,0] * m2 + c2, color='brown', label=f'Lower edge Ma={Ma2:.3f}')
+        plt.title("Frame " + timestep + " - " + type)
+        plt.legend()
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.savefig(f'plots/{type}/{timestep}')
+        plt.clf()
 
 def plotMach(Ma1, Ma2, type):
-    plt.plot(Ma1)
-    plt.plot(Ma2)
-
-    print(f"Ma = {Ma1[5:].mean():.3f} ± {Ma1[5:].std():.3f} for {type}")
-    print(f"Ma = {Ma2[5:].mean():.3f} ± {Ma2[5:].std():.3f} for {type}")
-
-    plt.xlabel('Timestep')
-    plt.ylabel('Mach number')
-    plt.title("Mach number over time - " + type)
-    plt.ylim(0, 10)
-    plt.savefig(f'plots/{type}/mach')
-    plt.clf()
+    av = np.mean(Ma1[skip_first:])
+    er = np.std(Ma1[skip_first:])
+    print(f"Upper Ma = {av:.3f} ± {er:.3f} for {type}, relative error is {er / av:.3f}")
+    av = np.mean(Ma2[skip_first:])
+    er = np.std(Ma2[skip_first:])
+    print(f"Lower Ma = {av:.3f} ± {er:.3f} for {type}, relative error is {er / av:.3f}")
+    if PLOT:
+        plt.plot(Ma1)
+        plt.plot(Ma2)
+        plt.xlabel('Timestep')
+        plt.ylabel('Mach number')
+        plt.title("Mach number over time - " + type)
+        plt.ylim(0, 10)
+        plt.savefig(f'plots/{type}/mach')
+        plt.clf()
+    else:
+        # print(Ma1)
+        # print(Ma2)
+        pass
 
 Ma_ion1 = []
 Ma_ion2 = []
@@ -103,20 +132,24 @@ for timestep in timesteps:
     phi = data_phi[timestep][()]
 
     edge1, edge2, data = getEdges(ion, 20, np.arange(0.9, 0.97, 0.01), 1, 60, 140)
-    Ma1, c1, m1 = getMach(edge1)
-    Ma_ion1.append(Ma1)
-    Ma2, c2, m2 = getMach(edge2)
-    Ma_ion2.append(Ma2)
+    if len(edge1) > 0:
+        Ma1, c1, m1 = getMach(edge1)
+        Ma_ion1.append(Ma1)
+    if len(edge2) > 0:
+        Ma2, c2, m2 = getMach(edge2)
+        Ma_ion2.append(Ma2)
+    if len(edge1) > 0 and len(edge2) > 0:
+        plotCones(data, edge1, edge2, c1, m1, c2, m2, Ma1, Ma2, timestep, 'ion density')
 
-    plotCones(data, edge1, edge2, c1, m1, c2, m2, Ma1, Ma2, timestep, 'ion density')
+    edge1, edge2, data = getEdges(ele, 20, np.arange(0.9, 0.97, 0.01), 1, 60, 140)
+    if len(edge1) > 0:
+        Ma1, c1, m1 = getMach(edge1)
+        Ma_ele1.append(Ma1)
+    if len(edge2) > 0:
+        Ma2, c2, m2 = getMach(edge2)
+        Ma_ele2.append(Ma2)
+    if len(edge1) > 0 and len(edge2) > 0:
+        plotCones(data, edge1, edge2, c1, m1, c2, m2, Ma1, Ma2, timestep, 'electron density')
 
-    edge1, edge2, data = getEdges(ele, 20, np.arange(0.9, 0.99, 0.01), 1, 60, 140)
-    Ma1, c1, m1 = getMach(edge1)
-    Ma_ele1.append(Ma1)
-    Ma2, c2, m2 = getMach(edge2)
-    Ma_ele2.append(Ma2)
-
-
-    plotCones(data, edge1, edge2, c1, m1, c2, m2, Ma1, Ma2, timestep, 'electron density')
 plotMach(Ma_ion1, Ma_ion2, 'ion density')
 plotMach(Ma_ele1, Ma_ele2, 'electron density')
